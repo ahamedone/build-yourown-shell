a90ee908c3a9807c50db5a468dffc1f5553eeccd
Azhar Ahamed Sahib <azhar@MacBookProM1.local>
2025-02-08 07:33:51 -0500

    implement exit 0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-
-        while(true) {
+        do {
+        	System.out.print("$ ");
         	Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
+            if("exit 0".equals(input)) {
+            	System.exit(0);
+            }
             System.out.println(input + ": command not found");
-            System.out.print("$ ");
-        }
+        } while (true);
         
     }
 }
