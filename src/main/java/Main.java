b18af8e1ed58c8629019860a0367f5aea18800b4
Azhar Ahamed Sahib <azhar@MacBookProM1.local>
2025-02-08 07:28:38 -0500

    REPL removed the \n

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
         while(true) {
         	Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            System.out.println(input + ": command not found\n");
+            System.out.println(input + ": command not found");
             System.out.print("$ ");
         }
         
