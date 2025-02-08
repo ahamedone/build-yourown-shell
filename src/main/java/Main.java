0e0fb042b7864c539a6151264d70f9d8b5580f33
Azhar Ahamed Sahib <azhar@MacBookProM1.local>
2025-02-08 07:27:27 -0500

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
 
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        System.out.println(input + ": command not found\n");
+        while(true) {
+        	Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input + ": command not found\n");
+            System.out.print("$ ");
+        }
+        
     }
 }
