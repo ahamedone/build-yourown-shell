b76a31c86f75c10d0597bd3fb752225810c7fcd6
Azhar Ahamed Sahib <azhar@MacBookProM1.local>
2025-02-19 21:39:52 -0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,14 @@ public class Main {
             	System.exit(0);
             } else if (null != input && input.startsWith("echo ")) {
             	System.out.println(input.replace("echo ", ""));
+            } else if (null != input && (input.startsWith("type"))) {
+        		String inputText = input.replaceFirst("type ","");
+
+            	if((input.startsWith("type echo")  || input.startsWith("type exit") || input.startsWith("type type"))) {
+                	System.out.println(inputText + " is a shell builtin");
+            	} else {
+            		 System.out.println(inputText + ": not found");
+            	}
             } else {
                 System.out.println(input + ": command not found");
             }
