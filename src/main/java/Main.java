e13adea3b3b9ecf322f00cc1e19d54a0cd704168
Azhar Ahamed Sahib <azhar.a@msn.com>
2025-03-08 11:04:36 -0500

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
@@ -12,18 +16,20 @@ public class Main {
         	System.out.print("$ ");
         	Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            String[] userCommands = input.split("\\s+");
+            String[] userCommands = input.split(" ");
             switch(userCommands[0]){
-                case "exit" -> System.exit(0);
-                case "echo" -> System.out.println(input.replace("echo ", ""));
+                case "exit", "exit;" -> System.exit(0);
+                //case "echo" -> System.out.println(String.join(" ", input.replace("echo ", "").replace("'","")));
+                case "echo" -> System.out.println(String.join(" ", handleSingleQuote(input.replace("echo ", ""), "'(.*)'|\\S+")));
                 case "type" -> handleTypeCommand(userCommands[1]);
                 case "pwd"  -> System.out.println(directoryName);
                 case "cd"   -> {
-                    if(userCommands[1].contains("~")){
+                    String userParam = input.replace("cd ", "");
+                    if(userParam.contains("~")){
                         String homeDirectory = System.getenv("HOME");
-                        userCommands[1] = userCommands[1].replace("~", homeDirectory);
+                        userParam = userParam.replace("~", homeDirectory);
                     }
-                    String path = getAbsolutePath(userCommands[1], directoryName);
+                    String path = getAbsolutePath(String.join(" ", handleSingleQuote(userParam)), directoryName);
                     if(Files.isDirectory(Path.of(path))){
                         directoryName = Path.of(path).normalize().toString();
                     } else {
@@ -36,14 +42,35 @@ public class Main {
                     if (path == null) {
                         System.out.println(command + ": command not found");
                     } else {
-                        String fullPath = path + input.substring(command.length());
-                        Process p = Runtime.getRuntime().exec(input.split(" "));
+                        String[] osCommands = getOsCommands(command, input.substring(command.length()));
+                        Process p = Runtime.getRuntime().exec(osCommands);
                         p.getInputStream().transferTo(System.out);
                     }
                 }
             }
         } while (true);
         
+    }
+
+    private static String[] getOsCommands(String command, String params){
+        List<String> userCommands = new ArrayList<>();
+        userCommands.add(command);
+        userCommands.addAll(handleSingleQuote(params));
+        return userCommands.toArray(new String[0]);
+    }
+
+    private static List<String> handleSingleQuote(String input){
+        return handleSingleQuote(input, null);
+    }
+
+    private static List<String> handleSingleQuote(String input, String regex){
+        List<String> userCommands = new ArrayList<>();
+        String aRegex = null == regex ? "'([^']*)'|\\S+" :  regex ;
+        Matcher matcher = Pattern.compile(aRegex).matcher(input);
+        while(matcher.find()){
+            userCommands.add((null == matcher.group(1)) ? matcher.group() : matcher.group(1).replace("'", ""));
+        }
+        return userCommands;
     }
 
     private static String getAbsolutePath(String directoryPath, String currentDirectory){
